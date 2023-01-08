03fcac9597c2dfdf92c7a105fff88657d1c2a4e5
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2023-01-08 14:54:47 +0100

    Day 18 part 2

--- src/day18.rs
+++ src/day18.rs
@@ -1,5 +1,5 @@
 use crate::common::get_input_lines;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet, VecDeque};
 
 #[derive(Copy, Clone, Eq, PartialEq, Hash)]
 struct Coord {
@@ -8,14 +8,14 @@ struct Coord {
     z: i32,
 }
 
-#[derive(Default)]
-struct Cube {
-    top: bool,
-    bottom: bool,
-    left: bool,
-    right: bool,
-    front: bool,
-    back: bool,
+#[derive(Default, Clone)]
+struct Cube<T> {
+    top: T,
+    bottom: T,
+    left: T,
+    right: T,
+    front: T,
+    back: T,
 }
 
 #[derive(Copy, Clone)]
@@ -28,16 +28,20 @@ enum Side {
     Back,
 }
 
+#[derive(Clone)]
 struct Puzzle {
-    cubes: HashMap<Coord, Cube>,
+    cubes: HashMap<Coord, Cube<bool>>,
 }
 
 #[allow(dead_code)]
 pub fn run() {
     let puzzle = Puzzle::parse(get_input_lines());
 
-    let result = part1(puzzle);
+    let result = part1(puzzle.clone());
     println!("Result (part 1): {result}");
+
+    let result = part2(puzzle);
+    println!("Result (part 2): {result}");
 }
 
 fn part1(mut puzzle: Puzzle) -> usize {
@@ -49,7 +53,52 @@ fn part1(mut puzzle: Puzzle) -> usize {
         }
     }
 
-    puzzle.cubes.values().map(|i| i.surface_area()).sum()
+    puzzle.cubes.values().map(|i| i.count(|&j| !j)).sum()
+}
+
+fn part2(mut puzzle: Puzzle) -> usize {
+    let min = Coord {
+        x: puzzle.cubes.keys().map(|i| i.x - 1).min().unwrap(),
+        y: puzzle.cubes.keys().map(|i| i.y - 1).min().unwrap(),
+        z: puzzle.cubes.keys().map(|i| i.z - 1).min().unwrap(),
+    };
+
+    let max = Coord {
+        x: puzzle.cubes.keys().map(|i| i.x + 1).max().unwrap(),
+        y: puzzle.cubes.keys().map(|i| i.y + 1).max().unwrap(),
+        z: puzzle.cubes.keys().map(|i| i.z + 1).max().unwrap(),
+    };
+
+    let mut visited = HashSet::new();
+    let mut to_visit = VecDeque::from([min]);
+
+    while let Some(coord) = to_visit.pop_front() {
+        if !visited.insert(coord) {
+            continue;
+        }
+
+        for adjacent in coord.adjacent() {
+            let coord = adjacent.1;
+
+            if coord.x < min.x
+                || coord.y < min.y
+                || coord.z < min.z
+                || coord.x > max.x
+                || coord.y > max.y
+                || coord.z > max.z
+            {
+                continue;
+            }
+
+            if let Some(cube) = puzzle.cubes.get_mut(&coord) {
+                cube.set(&adjacent.0.opposite(), true)
+            } else {
+                to_visit.push_back(coord)
+            }
+        }
+    }
+
+    puzzle.cubes.values().map(|i| i.count(|&j| j)).sum()
 }
 
 impl Puzzle {
@@ -140,8 +189,8 @@ impl Side {
     }
 }
 
-impl Cube {
-    fn set(&mut self, side: &Side, value: bool) {
+impl<T> Cube<T> {
+    fn set(&mut self, side: &Side, value: T) {
         match side {
             Side::Top => {
                 self.top = value;
@@ -164,18 +213,21 @@ impl Cube {
         }
     }
 
-    fn values(&self) -> [bool; 6] {
+    fn values(&self) -> [&T; 6] {
         [
-            self.top,
-            self.bottom,
-            self.left,
-            self.right,
-            self.front,
-            self.back,
+            &self.top,
+            &self.bottom,
+            &self.left,
+            &self.right,
+            &self.front,
+            &self.back,
         ]
     }
 
-    fn surface_area(&self) -> usize {
-        self.values().iter().filter(|&&i| !i).count()
+    fn count<P>(&self, predicate: P) -> usize
+    where
+        P: Fn(&T) -> bool,
+    {
+        self.values().iter().filter(|i| predicate(i)).count()
     }
 }
