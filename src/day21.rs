8eba2cf3acfa5fb30840e202a8179e811aa9bbcd
Lucas Trzesniewski <lucas.trzesniewski@gmail.com>
2023-02-26 19:41:33 +0100

    Day 21 part 2

--- src/day21.rs
+++ src/day21.rs
@@ -6,6 +6,7 @@ use std::hash::Hash;
 use std::iter;
 use std::str::FromStr;
 
+#[derive(Copy, Clone)]
 enum Op {
     Add,
     Sub,
@@ -13,17 +14,21 @@ enum Op {
     Div,
 }
 
+#[derive(Clone)]
 struct Monkey {
     name: String,
     job: MonkeyJob,
     value: Option<isize>,
 }
 
+#[derive(Clone)]
 enum MonkeyJob {
     Value(isize),
     Formula(String, Op, String),
+    Dunno,
 }
 
+#[derive(Clone)]
 struct Puzzle {
     monkeys: HashMap<String, Monkey>,
 }
@@ -46,8 +51,11 @@ trait Node {
 pub fn run() {
     let puzzle = Puzzle::parse(get_input_lines());
 
-    let result = part1(puzzle);
+    let result = part1(puzzle.clone());
     println!("Result (part 1): {result}");
+
+    let result = part2(puzzle);
+    println!("Result (part 2): {result}");
 }
 
 fn part1(mut puzzle: Puzzle) -> isize {
@@ -66,6 +74,67 @@ fn part1(mut puzzle: Puzzle) -> isize {
     puzzle.get_node("root").value.unwrap()
 }
 
+fn part2(mut puzzle: Puzzle) -> isize {
+    let mut human = puzzle.get_node_mut("humn");
+    human.job = MonkeyJob::Dunno;
+
+    let dag = DAG::new(puzzle.monkeys.values(), iter::once(String::from("root")));
+    let sorted = dag
+        .topo_sort()
+        .iter()
+        .rev()
+        .map(|i| i.name.clone())
+        .collect::<Vec<_>>();
+
+    for name in sorted {
+        puzzle.process(&name);
+    }
+
+    let root = puzzle.get_node("root");
+    let (name_a, name_b) = match &root.job {
+        MonkeyJob::Formula(a, _, b) => (a, b),
+        _ => panic!(),
+    };
+
+    let branch_a = puzzle.get_node(name_a);
+    let branch_b = puzzle.get_node(name_b);
+
+    let (known, unknown) = match (branch_a.value.is_some(), branch_b.value.is_some()) {
+        (true, false) => (branch_a, branch_b),
+        (false, true) => (branch_b, branch_a),
+        _ => panic!(),
+    };
+
+    let target_value = known.value.unwrap();
+
+    fn solve(puzzle: &Puzzle, key: &str, target: isize) -> isize {
+        let node = puzzle.get_node(key);
+
+        match &node.job {
+            MonkeyJob::Value(n) => *n,
+            MonkeyJob::Formula(a, op, b) => {
+                let a = puzzle.get_node(a);
+                let b = puzzle.get_node(b);
+
+                match (a.value, op, b.value) {
+                    (None, Op::Add, Some(b)) => solve(&puzzle, &a.name, target - b),
+                    (Some(a), Op::Add, None) => solve(&puzzle, &b.name, target - a),
+                    (None, Op::Sub, Some(b)) => solve(&puzzle, &a.name, target + b),
+                    (Some(a), Op::Sub, None) => solve(&puzzle, &b.name, a - target),
+                    (None, Op::Mul, Some(b)) => solve(&puzzle, &a.name, target / b),
+                    (Some(a), Op::Mul, None) => solve(&puzzle, &b.name, target / a),
+                    (None, Op::Div, Some(b)) => solve(&puzzle, &a.name, target * b),
+                    (Some(a), Op::Div, None) => solve(&puzzle, &b.name, a / target),
+                    _ => panic!(),
+                }
+            }
+            MonkeyJob::Dunno => target,
+        }
+    }
+
+    solve(&puzzle, &unknown.name, target_value)
+}
+
 impl Puzzle {
     fn parse(lines: Vec<String>) -> Puzzle {
         Self {
@@ -81,20 +150,25 @@ impl Puzzle {
 
     fn process(&mut self, name: &str) {
         let monkey = self.get_node(name);
-        let value = Some(match &monkey.job {
-            MonkeyJob::Value(n) => *n,
+        let value = match &monkey.job {
+            MonkeyJob::Value(n) => Some(*n),
             MonkeyJob::Formula(a, op, b) => {
-                let a = self.get_node(a).value.unwrap();
-                let b = self.get_node(b).value.unwrap();
+                let a = self.get_node(a).value;
+                let b = self.get_node(b).value;
 
-                match op {
-                    Op::Add => a + b,
-                    Op::Sub => a - b,
-                    Op::Mul => a * b,
-                    Op::Div => a / b,
+                if let (Some(a), Some(b)) = (a, b) {
+                    Some(match op {
+                        Op::Add => a + b,
+                        Op::Sub => a - b,
+                        Op::Mul => a * b,
+                        Op::Div => a / b,
+                    })
+                } else {
+                    None
                 }
             }
-        });
+            MonkeyJob::Dunno => None,
+        };
 
         self.get_node_mut(name).value = value;
     }
@@ -169,7 +243,7 @@ impl Node for Monkey {
 
     fn get_edges(&self) -> Vec<&Self::Key> {
         match &self.job {
-            MonkeyJob::Value(_) => vec![],
+            MonkeyJob::Value(_) | MonkeyJob::Dunno => vec![],
             MonkeyJob::Formula(a, _, b) => vec![&a, &b],
         }
     }
